00c4bce1585bf9b971093cfd81b82ac4746b6bc9
suwigyarathore <Suwi.Rathore@klm.com>
2018-08-11 15:31:18 +0200

    Implment menu via nav links

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    className="p1 mx2 black round text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
+
+HeaderLink.propTypes = {
+  children: PropTypes.node,
+};
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between px4">
     <h1 className="h1">🍽 MyRecipes</h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorites">Favorites</HeaderLink>
+    </nav>
   </header>
 );
 
